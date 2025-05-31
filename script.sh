f382a20e34afe063e2edfa57d92d42f577ddbdc1
Dina Gamal <106841244+DinaGamal13@users.noreply.github.com>
2025-05-31 15:19:33 +0300

    firstworkflow

---
+++ script.sh
@@ -0,0 +1,9 @@
+# sudo apt-get install cowsay -y
+# cowsay -f dragon "Run for cover, iam a Dragon....RAWR" >> dragon.txt
+
+
+sudo apt-get install cowsay -y
+cowsay -f dragon "Run for cover, i am a Dragon..... RAWR" >> dargon.txt
+grep -i "dragon" dragon.txt
+cat dragon.txt
+ls -ltra
