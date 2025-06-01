0ae60b34699d8f3bf95caa4eab5250dc52c7c53b
Dina Gamal <106841244+DinaGamal13@users.noreply.github.com>
2025-06-01 11:49:06 +0300

    run

--- script.sh
+++ script.sh
@@ -3,7 +3,7 @@
 
 
 sudo apt-get install cowsay -y
-cowsay -f dragon "Run for cover, i am a Dragon..... RAWR" >> dargon.txt
+cowsay -f dragon "Run for cover, i am a Dragon..... RAWR" >> dragon.txt
 grep -i "dragon" dragon.txt
 cat dragon.txt
 ls -ltra
